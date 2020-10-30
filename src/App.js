fc9ad226e7dbdb6a0274217e30bbf621c2ec33c6
Spiritusanti <mccrackenjacob@gmail.com>
2020-10-30 09:53:23 -0700

    started adding in firestore to create user objects

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -24,7 +24,9 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+      createUserProfileDocument(user);
+
+      console.log(user);
     })
   }
 
