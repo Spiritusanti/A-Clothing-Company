ecbc16a281585739b4c79866901ac29b1f90b206
Spiritusanti <mccrackenjacob@gmail.com>
2020-11-15 15:36:08 -0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Hnu8VAwnRvzesw2pH1EZtJcSSbhq2mJ0xERgOuTDEODgjgB9Od3NbAg77g8DzAua5vKVkYKW4AzgSp3rOc93MEW00DOyX91pX';
+
+   const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='A Clothing Company'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+
+export default StripeCheckoutButton;
